07562e680ab861d69869a41ff27ebd0469c69073
Pablo Vallejos <faktory.fm@live.com>
2024-02-07 14:25:08 -0300

    update de la tarea 2

--- ejercicios/ejercicio2_1.js
+++ ejercicios/ejercicio2_1.js
@@ -22,3 +22,18 @@ for (let i = 1; i <= 12; i++) {
 }
 
 //se ejecuta asi:  node tabla_multiplicar_argumento.js 7
+
+
+/*
+process: Es un objeto global en Node.js que proporciona información y control sobre el proceso de ejecución.
+argv: Es una propiedad de process que contiene una matriz con los argumentos de la línea 
+de comandos proporcionados al ejecutar el proceso Node.js.
+[2]: Accede al tercer elemento de la matriz argv, 
+ya que los primeros dos elementos son rutas al ejecutable de Node.js 
+y al script que se está ejecutando. En JavaScript, los índices de matriz comienzan en 0, 
+por lo que [2] hace referencia al tercer elemento.
+En resumen, const numero = process.argv[2];
+ establece una constante llamada numero 
+ que contiene el tercer argumento pasado al script Node.js al ejecutarlo desde la línea de comandos.
+  Por ejemplo, si ejecutas node script.js 10, numero tendrá el valor "10".
+*/
