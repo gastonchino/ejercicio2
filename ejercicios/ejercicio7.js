6bd89b81d3515cf9beb267055b69d868598181f7
Pablo Vallejos <faktory.fm@live.com>
2024-02-08 12:00:34 -0300

    ejercicios de prueba y de clase

--- ejercicios/ejercicio7.js
+++ ejercicios/ejercicio7.js
@@ -1,15 +1,13 @@
-/*
-Escribir un programa que declare un numero entero con cualquier valor e indique 
-si dicho número es par o impar.
-*/
-// Par o Impar
 
-// Declarar un número entero con cualquier valor
-const numero = 17; // Puedes cambiar este valor según tus necesidades
+const numero = 17; 
 
-// Verificar si el número es par o impar
 if (numero % 2 === 0) {
     console.log(`${numero} es un número par.`);
 } else {
     console.log(`${numero} es un número impar.`);
+}
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
 }
--- ejercicios/ejercicio8.js
+++ ejercicios/ejercicio8.js
@@ -1,15 +1,9 @@
-/*
-Escribir un programa que declare un numero entero del 1 al 7 y muestre por pantalla el dia de la semana correspondiente. Controlar que el numero se encuentre en el rango correcto, si no es asi,
- informar un error. Si el numero es 2 el dia es martes.
-*/
-// Día de la semana
 
-// Declarar un número entero del 1 al 7
-const numeroDia = 2; // Puedes cambiar este valor según tus necesidades
+const numeroDia = 2; // instanciar == crear 
 
-// Verificar si el número está en el rango correcto (1 al 7)
+
 if (numeroDia >= 1 && numeroDia <= 7) {
-    // Asignar el día de la semana correspondiente
+    
     let diaSemana;
 
     switch (numeroDia) {
@@ -38,9 +32,9 @@ if (numeroDia >= 1 && numeroDia <= 7) {
             break;
     }
 
-    // Mostrar el día de la semana
+    
     console.log(`El número ${numeroDia} corresponde a ${diaSemana}.`);
 } else {
-    // Informar un error si el número está fuera del rango
+    
     console.log("Error: El número debe estar en el rango del 1 al 7.");
 }
