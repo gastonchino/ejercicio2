8b99e04e448bc24117a069784fd4c075adf62b41
Pablo Vallejos <faktory.fm@live.com>
2024-02-21 11:28:00 -0300

    correcion clase4.1

--- gaston-tarea/tareaClase4.1/tare3/esBisiesto_O_noF.js
+++ gaston-tarea/tareaClase4.1/tare3/esBisiesto_O_noF.js
@@ -1,6 +1,7 @@
 var año = 2024; 
 AñoBisiesto_oNo(año)
-
+// camel case  no es AñoBisiesto_oNo no es un verbo y no es camel case 
+// getVerifyLeapYear() obtenerVerificacionAñoBisiesto() un ejemplo
 function AñoBisiesto_oNo(año){
     if ((año % 4 === 0 && año % 100 !== 0) || (año % 400 === 0)) {
         console.log(año + " es un año bisiesto");
--- gaston-tarea/tareaClase4.1/tarea2/EsPositivoOnegativoF.js
+++ gaston-tarea/tareaClase4.1/tarea2/EsPositivoOnegativoF.js
@@ -1,6 +1,7 @@
 let numero = 4;
 esPositivo_oNegativo(numero)
-
+// camel case  no es esPositivo_oNegativo no es un verbo y no es camel case
+//getVerifyTypeNumber() obtenerVerificacionDetipoDeNumero()
 function esPositivo_oNegativo(numero){
     if( numero > 0) {
         console.log("El número es positivo")
